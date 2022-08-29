7c14e9de450e80821dc55911012719fd0f77a338
Karan Bharda <karanbharda023@gmail.com>
2022-08-30 03:59:12 +0530

    final commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.type', {
-    strings: ['Student', 'Programmer','Web-Devloper','Cyber-Security-Enthusiast','Gamer'],
+    strings: ['Student', 'Programmer', 'Web-Devloper', 'Cyber-Security-Enthusiast', 'Gamer'],
     typeSpeed: 60,
     backSpeed: 60,
     loop: true
