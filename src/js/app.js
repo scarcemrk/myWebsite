b744193ce6ca4e1d1193daae7032232e6495ab91
Karan Bharda <karanbharda023@gmail.com>
2022-08-31 02:04:01 +0530

    make site responsive

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.type', {
-    strings: ['Student', 'Programmer', 'Web-Devloper', 'Cyber-Security-Enthusiast', 'Gamer'],
+    strings: ['Student', 'Programmer', 'Web-Devloper', 'Ethical-Hacker', 'Gamer'],
     typeSpeed: 60,
     backSpeed: 60,
     loop: true
